73d5f0c89254e0505e25c4127c25b1c961647d27
pawan-kavinda <pawankavinda09@gmail.com>
2025-09-23 01:48:48 +0530

    dto

--- src/api/chat/dto/chat-message.dto.ts
+++ src/api/chat/dto/chat-message.dto.ts
@@ -1,9 +1,21 @@
 import { IsEmail, IsString, MinLength } from "class-validator";
+import { Agent } from "http";
+import { AgentTypes } from "src/constants/agent-types";
 
 export class IncomingChatMessageDto {
   @IsString()
   message: string;
   @IsString()
   sessionId: string;
+
+  @IsString()
+  company_uuid: string;
+
+  @IsString()
+  top_k: number;
+
+  @IsString()
+  type?: AgentTypes;
+
   file?: File;
 }
