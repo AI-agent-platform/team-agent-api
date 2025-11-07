499a6aabbe70f58082dbef8fee1264051b17f6f7
pawan-kavinda <pawankavinda09@gmail.com>
2025-11-07 21:25:52 +0530

    chat service inject

--- src/api/business/business.module.ts
+++ src/api/business/business.module.ts
@@ -5,6 +5,8 @@ import { BusinessService } from "./business.service";
 import { Business, BusinessSchema } from "./model/business.model";
 import { User, UserSchema } from "../user/model/user.model";
 import { EmailService } from "../email/email.service";
+import { MetaModule } from "../meta/meta.module";
+import { ChatModule } from "../chat/chat.module";
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { EmailService } from "../email/email.service";
       { name: Business.name, schema: BusinessSchema },
       { name: User.name, schema: UserSchema },
     ]),
+    MetaModule,
+    ChatModule
   ],
   controllers: [BusinessController],
   providers: [BusinessService, EmailService],
