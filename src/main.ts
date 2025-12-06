c2b056777bd6ee07e300468369e09ded56f4c6eb
pawan-kavinda <pawankavinda09@gmail.com>
2025-12-06 20:57:54 +0530

    revamp

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: (origin, cb) => cb(null, true),
     credentials: true,
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type, Authorization",
   });
   await app.listen(process.env.PORT);
   console.log(`Server is running on port ${process.env.PORT}`);
