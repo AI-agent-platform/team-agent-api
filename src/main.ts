49e3a12bc2b287ac62181d992416a2960a5f721d
pawan-kavinda <pawankavinda09@gmail.com>
2025-12-07 01:07:08 +0530

    cors issue check

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://icy-desert-09c5afe00.3.azurestaticapps.net",
+  ];
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      "http://localhost:3000",
-      "https://icy-desert-09c5afe00.3.azurestaticapps.net",
-    ],
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);  
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`Origin ${origin} not allowed by CORS`));
+      }
+    },
     credentials: true,
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
