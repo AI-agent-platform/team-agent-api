7bc34b4f4559df198cc41f3f1fd84c3bf0808cae
pawan-kavinda <pawankavinda09@gmail.com>
2025-12-06 21:01:02 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: (origin, cb) => cb(null, true),
+    origin: [
+      "http://localhost:3000",
+      "https://icy-desert-09c5afe00.3.azurestaticapps.net",
+    ],
     credentials: true,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
   });
+
   await app.listen(process.env.PORT);
   console.log(`Server is running on port ${process.env.PORT}`);
 }
