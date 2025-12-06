5c992e9b8a789ccdb3d3cd9295ab33f36c0aec52
pawan-kavinda <pawankavinda09@gmail.com>
2025-12-06 20:46:53 +0530

    changed cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
+    origin: true,
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
   });
   await app.listen(process.env.PORT);
   console.log(`Server is running on port ${process.env.PORT}`);
